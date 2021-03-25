37702fa5b9f18c2700a35a4d72230b3f63383719
Josh Rehanek <joshrehanek@gmail.com>
2021-03-25 16:41:57 -0700

    adds dates and weather divs

--- src/App.js
+++ src/App.js
@@ -7,11 +7,36 @@ const apiKey = {
 }
 
 export default function App() {
+
+  const handleDate = (d) => {
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
   return (
     <div className="App">
       <main>
         <div className="search-area">
           <input type="text" className="search-bar" placeholder="City" />
+        </div>
+        <div className="location-area">
+          <div className="location">Suva, Fiji</div>
+          <div className="date">{handleDate(new Date())}</div>
+        </div>
+        <div className="weather-area">
+          <div className="temp">
+              82 F
+          </div>
+          <div className="weather">
+              Sunny
+          </div>
         </div>
       </main>
     </div>
