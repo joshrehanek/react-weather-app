6a12f11d5d78dc698b4f6d6438ddd0164fb4d6b7
Josh Rehanek <joshrehanek@gmail.com>
2021-03-24 23:36:54 -0700

    adds search area

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react"
+import "../src/styles.css";
+
+const apiKey = {
+  key: "1236caef6b9134318c4caad26620bb42",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <main>
+        <div className="search-area">
+          <input type="text" className="search-bar" placeholder="City" />
+        </div>
+      </main>
     </div>
   );
 }
